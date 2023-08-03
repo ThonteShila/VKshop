f9d865cf3dd9f4e8e6cf1dc1a09b7278f7a5287e
Thonteshila <thonte.shila>
2023-08-04 01:06:00 +0530

    table created

--- vkshop/vkshop/settings.py
+++ vkshop/vkshop/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'vkshop_app',
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- vkshop/vkshop_app/models.py
+++ vkshop/vkshop_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
 # Create your models here.
+class Category(models.Model):
+    category_name=models.CharField(max_length=200)
+    slug=models.SlugField(unique=True,null=True,blank=True)                              #slug-in http ling we can give valid name
+    category_image=models.ImageField(upload_to="categories")
+
+class Product(models.Model):
+    product_name=models.CharField(max_length=200)
+    slug=models.SlugField(unique=True,null=True,blank=True)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE,related_name="products")
+    price=models.IntegerField()
+    product_description=models.TextField()
+
+class ProductImage(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.CASCADE,related_name="product_image")
+    image=models.ImageField(upload_to="product")
